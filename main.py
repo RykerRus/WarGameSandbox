c3c071e50eec7ad48589eed7066847957786ce85
Lipkin Alex <lipkin01@yandex.ru>
2017-11-20 16:18:28 +0300

    v0.0.0.1

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Импортируем библиотеку pygame
 import pygame
 from pygame.locals import *
-
+import Cage
 #Объявляем переменные
 WIN_WIDTH = 800 #Ширина создаваемого окна
 WIN_HEIGHT = 600 # Высота
@@ -10,7 +10,7 @@ BACKGROUND_COLOR = "#004400"
 PLATFORM_WIDTH = 32
 PLATFORM_HEIGHT = 32
 PLATFORM_COLOR = "#FF6262"
-cage = pygame.image.load('image/cage.bmp')
+cage = Cage.cage()
 background = [[cage, cage, cage, cage, cage, cage, cage, cage],
               [cage, cage, cage, cage, cage, cage, cage, cage],
               [cage, cage, cage, cage, cage, cage, cage, cage],
@@ -33,7 +33,7 @@ def main():
         for a in range(len(background)):
             b = a * 100
             for i in range(len(background[a])):
-                screen.blit(background[a][i], (i*100, b))
+                screen.blit(background[a][i].load_image(), (i*100, b))
         pygame.display.update()
 
 main()
